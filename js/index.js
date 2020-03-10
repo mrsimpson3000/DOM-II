5beac7d9bc2f217333ebdd30856d82569f4bc3bb
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-03-10 19:42:23 -0400

    Finally found a sound I liked

--- js/index.js
+++ js/index.js
@@ -20,15 +20,24 @@
 // Above was from today's lesson. My code begions now.
 function makeItBold(event) {
   event.target.style.fontWeight = 'bold';
-  setTimeout(function(){
+  setTimeout(function () {
     event.target.style.fontWeight = 'normal';
-  }, 500) 
+  }, 500)
 }
+
+function playSound(event) {
+  const audio = new Audio("../audio/carstartgarage.mp3");
+  audio.play();
+  // console.log('called playSound');
+  event.stopPropagation();
+}
+
+// Makes any H2 you hover over bold for a moment
 const allH2s = document.querySelectorAll('h2');
-console.log(allH2s);
 for (let i = 0; i < allH2s.length; i++) {
-  console.log(allH2s[i]);
+  // console.log(allH2s[i]);
   allH2s[i].addEventListener('mouseover', makeItBold);
 }
-  
 
+// If you click the fun bus img it plays a sound
+document.querySelector(".intro > img").addEventListener('click', playSound);
