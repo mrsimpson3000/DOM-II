6855cfa2ffd476f8c5af9ddb2dd4a9fab4f5cb0f
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-03-10 15:29:38 -0400

    Commented out all changes made during instruction and starting now

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
-// Your code goes here
+// using camelCase id as variable in the code
+// to work on the home link
+
+// function clickEventHandler(event) {
+//   console.log(event.type);
+//   console.log(event.target);
+//   console.log(event.currentTarget);
+// }
+
+// homeLink.addEventListener('click', clickEventHandler)
+
+// document.querySelector('nav').addEventListener('click',clickEventHandler
+// )
+
+// document.querySelector('header').addEventListener('click', clickEventHandler
+// )
+
+// document.body.addEventListener('click',clickEventHandler)
+
+// Above was from today's lesson. My code begions now.
