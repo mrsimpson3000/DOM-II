1d94c455f6c46d837f46d07da43044218812ea52
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-03-10 20:31:01 -0400

    4 events done. Why is it so hard to find events to use?

--- js/index.js
+++ js/index.js
@@ -18,19 +18,7 @@
 // document.body.addEventListener('click',clickEventHandler)
 
 // Above was from today's lesson. My code begions now.
-function makeItBold(event) {
-  event.target.style.fontWeight = 'bold';
-  setTimeout(function () {
-    event.target.style.fontWeight = 'normal';
-  }, 500)
-}
 
-function playSound(event) {
-  const audio = new Audio("../audio/carstartgarage.mp3");
-  audio.play();
-  // console.log('called playSound');
-  event.stopPropagation();
-}
 
 // Makes any H2 you hover over bold for a moment
 const allH2s = document.querySelectorAll('h2');
@@ -39,5 +27,37 @@ for (let i = 0; i < allH2s.length; i++) {
   allH2s[i].addEventListener('mouseover', makeItBold);
 }
 
+function makeItBold(event) {
+  event.target.style.fontWeight = 'bold';
+  setTimeout(function () {
+    event.target.style.fontWeight = 'normal';
+  }, 500)
+}
+
+// Alert the user only once to start the bus 
+funBusImg.addEventListener('mouseleave', startMe);
+
+function startMe(event) {
+  console.log('called startMe');
+  event.target.removeEventListener(event.type, arguments.callee);
+  alert('Click the van to start it!');
+}
+
 // If you click the fun bus img it plays a sound
 document.querySelector(".intro > img").addEventListener('click', playSound);
+
+function playSound(event) {
+  const audio = new Audio("../audio/carstartgarage.mp3");
+  audio.play();
+  // console.log('called playSound');
+  event.stopPropagation();
+}
+
+// Double clicking an image hides it temporarily
+adventureImage.addEventListener('dblclick', function (event) {
+  console.log('hide me')
+  event.target.style.display = 'none';
+  setTimeout(function () {
+    event.target.style.display = 'block';
+  }, 700)
+})
